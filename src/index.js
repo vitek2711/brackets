4557de386f3d4d9221a18bf4d9947f5d442941f8
vitek2711 <viktor2711@mail.ru>
2022-08-01 00:53:39 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
+// const bracketsConfig1 = [['(', ')']];
+// const bracketsConfig2 = [['(', ')'], ['[', ']']];
+// const bracketsConfig = [['(', ')'], ['[', ']'], ['{', '}']];
+// const bracketsConfig4 = [['|', '|']];
+// const bracketsConfig5 = [['(', ')'], ['|', '|']];
+// const bracketsConfig = [['1', '2'], ['3', '4'], ['5', '6'], ['7', '7'], ['8', '8']];
+// let str = '88888778788877777788888887777777887887788788887887777777788888888887788888'
+
+module.exports =function check(str, bracketsConfig) {
+    let stackItems = [];
+    bracketsConfig.map((e) => {
+        stackItems.push(e.join(""));
+    });
+    // console.log(stackItems);
+
+    for (let j = 0; j < stackItems.length; j++) {
+        if (str.includes(stackItems[j])) {
+            str = str.replace(stackItems[j], "");
+            j = -1;
+            // console.log(str);
+        };
+    };
+    return str.length === 0;
 }
+
+// check(str, bracketsConfig)
